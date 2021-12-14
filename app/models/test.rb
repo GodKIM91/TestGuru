6e2d1cc9cf157a0451bf81e2109380388f477dff
GodKIM91 <godkimovsk@gmail.com>
2021-12-14 22:21:27 +0300

    Lesson 6

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,11 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-
-  def self.by_category(category_name)
-    joins(:category).where(categories: {title: category_name}).order(title: :desc).pluck(:title)
-  end
+  validates :title, presence: true,
+                    uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than: -1 }
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_category, -> (category_name) { joins(:category).where(categories: {title: category_name}).order(title: :desc).pluck(:title) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
+  validates :email, presence: true
 
   def sort_user_tests_by_level(level)
     self.tests.where(level: level)
