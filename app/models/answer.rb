c7d0f58b02f3961db5a4cf5a0a9409fa7a2265ed
GodKIM91 <godkimovsk@gmail.com>
2021-12-21 23:53:37 +0300

    refactoring lesson 6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :question
   validates :body, presence: true
   validate :validate_answers_qty, on: :create
-  scope :return_correct, -> { where(correct: true) }
+  scope :correct_answers, -> { where(correct: true) }
 
   private
   def validate_answers_qty
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,14 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   validates :title, presence: true,
                     uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than: -1 }
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :by_category, -> (category_name) { joins(:category).where(categories: {title: category_name}).order(title: :desc).pluck(:title) }
+  scope :by_category, -> (category_name) { joins(:category).where(category: {title: category_name}) }
 end
