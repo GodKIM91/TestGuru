dda8af91562062ed0be5062b18f55087ada89de5
GodKIM91 <godkimovsk@gmail.com>
2022-01-16 21:08:39 +0300

    lesson 9 app logic

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 class TestsController < ApplicationController
 
-  before_action :find_test, only: %i[show destroy edit update]
+  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_user, only: :start
 
   def index
     @tests = Test.all
@@ -38,14 +39,23 @@ class TestsController < ApplicationController
     redirect_to tests_path
   end
 
+  def start
+    @user.tests.push(@test)
+    redirect_to @user.test_passage(@test)
+  end
+
   private
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id, :author_id)
   end
 
-  def find_test
+  def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_user
+    @user = User.first
   end
 
 end
