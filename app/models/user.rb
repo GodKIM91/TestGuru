56a2c0382453a8a8d847649ce60f8af0804d3c92
GodKIM91 <godkimovsk@gmail.com>
2022-01-18 21:18:54 +0300

    Lesson 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
   validates :email, presence: true
+  validates :email, uniqueness: { message: 'already exists' },
+                    format: { with: URI::MailTo::EMAIL_REGEXP,
+                    message: "Wrong email format, use pattern 'sometext@mail.com'" }
+
+  has_secure_password
 
   def sort_user_tests_by_level(level)
     self.tests.where(level: level)
