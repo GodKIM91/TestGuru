fffb38f2c481af0483e85c57be61037382ee6203
GodKIM91 <godkimovsk@gmail.com>
2022-01-20 12:15:37 +0300

    fixed db: email unique true

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2022_01_19_113220) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
