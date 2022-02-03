827571bcb5f62db7cc5072508b5b74f05d1de4ea
GodKIM91 <godkimovsk@gmail.com>
2022-02-03 23:14:11 +0300

    Lesson 13

--- Gemfile
+++ Gemfile
@@ -27,6 +27,11 @@ gem 'devise', '~> 4.0'
 
 gem 'rails-i18n'
 
+#UI
+
+gem 'jquery-rails'
+gem 'bootstrap', '~> 5.1.3'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'TestGuru, dev by: Alexander Melekhin', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_class(type)
+    types = {
+      "notice" => "alert alert-info", "success" => "alert alert-success",
+      "error" => "alert alert-warning", "alert" => "alert alert-warning"
+    }
+    types[type]
+  end
 end
